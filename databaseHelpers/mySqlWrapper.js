b08a036fa973f94e5ba89cf11691e3c846065eff
frankmlve <41298080+frankmlve@users.noreply.github.com>
2020-07-14 15:13:15 -0400

    Update mySqlWrapper.js

--- databaseHelpers/mySqlWrapper.js
+++ databaseHelpers/mySqlWrapper.js
@@ -1,46 +1,31 @@
 module.exports = {
-  query: query
-}
-const dotenv = require('dotenv');
-const POOL = require('pg').Pool;
-var pool;
-dotenv.config();
+  query: query,
+};
 
-function initConnection() {
-   pool = new POOL({
+const {
+  Pool
+} = require('pg')
+const connectionString = process.env.DATABASE_URL
+
+function query(queryString, cbFunc) {
+  const pool = new Pool({
+    /* user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    ssl: true */
+    connectionString: connectionString,
+  });
 
-  })
+  pool.query(queryString, (error, results) => {
+    cbFunc(setResponse(error, results));
+  });
 }
 
-/**
- * executes the specified sql query and provides a callback which is given
- * with the results in a DataResponseObject
- *
- * @param queryString
- * @param callback - takes a DataResponseObject
- */
-function query(queryString, callback) {
-  initConnection()
-  pool.query(queryString, function (error, results, fields) {
-    callback(setResponse(error, results))
-  })
-}
-
-/**
- * creates and returns a DataResponseObject made out of the specified parameters.
- * A DataResponseObject has two variables. An error which is a boolean and the results of the query.
- *
- * @param error
- * @param results
- * @return {DataResponseObject<{error, results}>}
- */
 function setResponse(error, results) {
   return {
     error: error,
-    results: results === undefined ? null : results === null ? null : results
-  }
+    results: results ? results : null,
+  };
 }
