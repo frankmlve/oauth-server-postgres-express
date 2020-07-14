8d32fe1f4c0abc6ba8dc1a2dcff86bb2b8ad3b5b
frankmlve <41298080+frankmlve@users.noreply.github.com>
2020-07-14 10:18:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -45,6 +45,4 @@ expressApp.use('/restrictedArea', restrictedAreaRoutes)
 //MARK: --- INITIALISE MIDDLEWARE & ROUTES
 
 //init the server
-expressApp.listen(process.env.SERVER_PORT || 8080, function(){
-  console.log("Express server is listening on port %d in %s mode", this.address().port, expressApp.settings.env);
-})
+expressApp.listen(process.env.SERVER_PORT || 500)
