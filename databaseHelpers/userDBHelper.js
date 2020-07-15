e0c795e1a9ca377c4cfdcbee15422f147d61c7b7
frankmlve <41298080+frankmlve@users.noreply.github.com>
2020-07-15 08:22:17 -0400

    Update userDBHelper.js

--- databaseHelpers/userDBHelper.js
+++ databaseHelpers/userDBHelper.js
@@ -22,7 +22,7 @@ module.exports = injectedMySqlConnection => {
  * @param password
  * @param registrationCallback - takes a DataResponseObject
  */
-let current_date = new Date();
+let current_date = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
 function registerUserInDB(username, password, registrationCallback) {
 
