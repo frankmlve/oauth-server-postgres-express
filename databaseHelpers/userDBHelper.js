7cd2752f1d200a3bae5fcef8466dba0e3e908aa0
frankmlve <41298080+frankmlve@users.noreply.github.com>
2020-07-15 09:14:21 -0400

    Update userDBHelper.js

--- databaseHelpers/userDBHelper.js
+++ databaseHelpers/userDBHelper.js
@@ -23,11 +23,11 @@ var crypto = require("crypto");
  * @param registrationCallback - takes a DataResponseObject
  */
 //let current_date = new Date().toISOString().slice(0, 19);
-let current_date = new Date()
+let current_date = new Date().toISOString();
 function registerUserInDB(username, password, registrationCallback) {
 var shaPass = crypto.createHash("sha256").update(password).digest("hex");
   //create query using the data in the req.body to register the user in the db
-  const registerUserQuery = `INSERT INTO "users" (username, password, created_date) VALUES ('${username}', '${shaPass}', ${current_date}::timestamp)`
+  const registerUserQuery = `INSERT INTO "users" (username, password, created_date) VALUES ('${username}', '${shaPass}', ${current_date})`
 console.log('Query para insert= '+registerUserQuery)
   //execute the query to register the user
   mySqlConnection.query(registerUserQuery, registrationCallback)
