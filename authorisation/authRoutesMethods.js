d70b1d8dba67576903180fd41c2dbf1deb91f0a8
franklin.lucena89@gmail.com <franklin.lucena89@gmail.com>
2020-07-20 07:24:43 -0400

    update

--- authorisation/authRoutesMethods.js
+++ authorisation/authRoutesMethods.js
@@ -119,10 +119,10 @@ function sendEmailWithNewToken(username, user_id, app_url, token, res) {
   var transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT),
-    secure: false,
+/*     secure: false,
     tls: {
       rejectUnauthorized: true
-    },
+    }, */
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
