496fa742676afc5389d2763f62d82c33f1172e0e
franklin.lucena89@gmail.com <franklin.lucena89@gmail.com>
2020-07-22 11:17:49 -0400

    update

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // @ts-check
 
 const config = {
-    endpoint: "https://oauth-gihsa-portalaccionista-db.documents.azure.com:443/",
-    key: "mFTlxPH8zZfvlN3526BEqODD689jXNfourhQWUef1tCwzyjmRccIvSfKELaHfLPYssIjWMgU8lk22ecvlwBMwA==",
-    databaseId: "userdirectory",
-    containerId: "users",
-    partitionKey: { kind: "Hash", paths: ["/users"] }
+    endpoint: process.env.DATABASE_HOST,
+    key: process.env.DATABASE_PASSWORD,
+    databaseId: process.env.DATABASE_NAME,
+    containerId: process.env.DATABASE_CONTAINER,
+    partitionKey: { kind: "Hash", paths: [process.env.DATABASE_PATH] }
   };
   
   module.exports = config;
